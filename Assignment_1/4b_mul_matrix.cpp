eeb4c149edafdc635f76e1aaa8f5dd993ae6f462
Your Suhkpree-t <sukhpreet08072005@gmailcom>
2025-08-13 14:34:15 +0530

    upload

--- Assignment_1/4b_mul_matrix.cpp
+++ Assignment_1/4b_mul_matrix.cpp
@@ -7,6 +7,19 @@ int main() {
     int arr1[n][m] = {{1,2},{3,4}};
     int arr2[n][m] = {{5,6},{7,8}};
     int arr[n][m];
+    //Input the size of the matrices
+    for (int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            cin >> arr1[i][j];
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            cin >> arr2[i][j];
+        }
+    }
+
+    //Matrix multiplication
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < m; j++) {
             arr[i][j] = 0;
@@ -15,6 +28,22 @@ int main() {
             }
         }
     }
+    //Print the input matrices
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<m; j++) {
+            cout << arr1[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<m; j++) {
+            cout << arr2[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+    
     // Print the result
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < m; j++) {
