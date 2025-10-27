48d1c036e68e459a3f1cc2a8bf236d339aa430ec
Sukhpree-t <sukhpreet08072005@gmail.com.com>
2025-10-27 21:40:29 +0530

    update

--- Assignment_6/1a_doublelist.cpp
+++ Assignment_6/1a_doublelist.cpp
@@ -8,7 +8,8 @@ class Node {
     Node* prev;
     Node(int val) {
         data = val;
-        next = prev = NULL;
+        next = NULL;
+        prev = NULL;
     }
 };
 
@@ -16,11 +17,10 @@ class DoublyList {
     Node* head;
     Node* tail;
     public:
-
     DoublyList() {
         head = tail = NULL;
     }
-    
+
     bool isEmpty() {
         if(head==NULL) {
             return true;
@@ -28,19 +28,19 @@ class DoublyList {
         return false;
     }
 
-    void push_front(int val) {
+     void pushfront(int val) {
         Node* newNode = new Node(val);
         if(isEmpty()) {
             head = tail = newNode;
+        } else {
+        newNode->next = head;
+        head->prev = newNode;
+        head = newNode;
         }
-        else {
-            newNode->next = head;
-            head->prev = newNode;
-            head = newNode;
-        }
-    }
+        return;
+     }
 
-    void push_back(int val) {
+     void pushback(int val) {
         Node* newNode = new Node(val);
         if(isEmpty()) {
             head = tail = newNode;
@@ -50,76 +50,104 @@ class DoublyList {
             tail->next = newNode;
             tail = newNode;
         }
+        return;
+     }
+
+    void pushAftervalue(int val, int num) {
+        Node* newNode = new Node(num);
+        Node* temp = head;
+        while(temp!=NULL) {
+            if(temp->data == val) {
+                newNode->next = temp->next;
+                newNode->prev = temp;
+                if(temp->next!=NULL) {
+                    temp->next->prev = newNode;
+                }
+                temp->next = newNode;
+                return;
+            }
+            temp= temp->next;
+        }
+        cout<<val<<" : Value not found in the list"<<endl;
     }
 
-    void pop_front() {
+     void popfront() {
         if(isEmpty()) {
             cout<<"List is Empty"<<endl;
         }
         else {
             Node* temp = head;
             head = head->next;
-            if(head != NULL) {
-                head->prev = NULL;
-            } else {
-                // List becomes empty, so tail should also be NULL
-                tail = NULL;
-            }
+            head->prev = NULL;
             temp->next = NULL;
             delete temp;
         }
-    }
+     }
 
-    void pop_back() {
+     void popback() {
         if(isEmpty()) {
             cout<<"List is Empty"<<endl;
         }
         else {
-            Node* temp = tail;
-            tail = tail->prev;
-            if(tail != NULL) {
-                tail->next = NULL;
-            } else {
-                // List becomes empty, so head should also be NULL
-                head = NULL;
+            Node* curr = head;
+            while(curr->next != NULL) {
+                curr = curr->next;
             }
-            temp->prev = NULL;
-            delete temp;
+            if(curr == head) {
+                head = tail = NULL;
+            }
+            else {
+            tail = curr->prev;
+            curr->prev->next = NULL;
+            curr->prev = NULL;
+            }
+            delete curr;
+        }
+     }
+
+    void popVal(int val) {
+        Node* temp = head;
+        while(temp!=NULL) {
+            if(temp->data == val){
+                if(temp == head) {
+                    popfront();
+                }
+                else if(temp->next==NULL) {
+                    popback();
+                }
+                else {
+                    temp->prev->next = temp->next;
+                    temp->next->prev = temp->prev;
+                    delete temp;
+                }
+                
+                return;
+            }
+            temp = temp->next;
         }
     }
 
-    void printll() {
+     void printll() {
         Node* temp = head;
         while(temp != NULL) {
-            cout<< temp->data<<" -> ";
+            cout<< temp->data <<" -> ";
             temp = temp->next;
         }
         cout<<"NULL"<<endl;
-    }
+     }
+
 };
 
 int main() {
     DoublyList dll;
-
-    dll.push_front(2);
-    dll.push_front(4);
-    dll.push_front(6);
-
+    dll.pushfront(2);
+    dll.pushfront(4);
+    dll.pushback(5);
+    dll.printll();
+    dll.pushAftervalue(5,6);
+    dll.printll();
+    dll.popVal(6);
     dll.printll();
 
-    dll.push_back(8);
-
-    dll.printll();
-
-    dll.pop_front();
-    dll.printll();
-
-    dll.pop_back();
-    dll.pop_back();
-    // dll.pop_back();
-    dll.printll();
-
-    dll.pop_front();
-    dll.printll();
-
+    return 0;
 }
